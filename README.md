a406175fa212be32d9deb2e61e2c0128de991e20
crazy23lt <2394630102@qq.com>
2019-02-26 14:37:45 +0800

    init Message

---
+++ README.md
@@ -0,0 +1,3 @@
+# 使用 mongodb 制作简易留言模板
+git remote add origin git@github.com:crazy23lt/Message.git
+git push -u origin master
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const formidable = require('formidable')
+const app = express()
+const db = require('./model/db')
+app.set('view engine','ejs')
+app.use(express.static('./public'))
+app.get('/',(req,res,next)=>{
+    res.render('index')
+})
+app.post('/send',(req,res,next)=>{
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        db.insert('Message', [
+            { 'name': fields.name, 'message': fields.message ,'time': new Date()}
+        ], (err, result) => {
+            if (err) {
+                res.json('失败')
+                return
+            } else {
+                res.json('成功')
+            }
+        })
+    });
+})
+app.get('/find',(req,res)=>{
+    let page = req.query.page
+    db.find('Message',{},(err,result)=>{
+        res.json(JSON.stringify(result))
+    })
+})
+app.get('/count',(req,res)=>{
+    db.count('Message',{},(err,result)=>{
+        res.send(result)
+    })
+})
+app.listen(3000)
